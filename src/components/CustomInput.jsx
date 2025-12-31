ed2ad22403f6ac3755a81784c3ac08a65e652fb4
Gabriel Nunes <gabrielnunesdev@outlook.com>
2025-12-30 23:59:28 -0300

    fix: update CustomInput to bind value prop for controlled input

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -3,7 +3,12 @@ import "./CustomInput.scss";
 const CustomInput = ({ label, value, onChange }) => {
   return (
     <div className="custom-input-container">
-      <input type="text" className="custom-input" onChange={onChange} />
+      <input
+        type="text"
+        className="custom-input"
+        value={value}
+        onChange={onChange}
+      />
       {label ? (
         <label
           className={`${value.length > 0 ? "shrink" : ""} custom-input-label`}
